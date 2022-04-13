34ec972c3eb049c1bf5c32115147415fe22854d4
riferrei <riferrei@riferrei.com>
2022-04-13 19:51:30 -0400

    Removed Elastic references

--- recover-dumps.sh
+++ recover-dumps.sh
@@ -2,7 +2,7 @@
 
 docker run --net=host --rm -ti -v ${PWD}/dumps:/tmp elasticdump/elasticsearch-dump \
   --input=/tmp/events.json \
-  --output=http://admin:changeme@localhost:9200/events \
+  --output=http://localhost:9200/events \
   --type=data \
   --timeout=1000 \
   --overwrite
